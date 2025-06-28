7f52c8b8ba0b167dba9b0512728aa5a8a3bc5aff
Arshad Hussain <ajh200131@gmail.com>
2025-06-27 20:10:04 -0400

    Add song names

--- app/src/Group.js
+++ app/src/Group.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Group = () => {
-  const { groupName } = useParams();
+  const [songNames, setSongNames] = useState([]);
+  const { groupName } = useParams()
+  const searchParams = new URLSearchParams({
+    'group_name': groupName
+  }).toString()
+  
+  fetch('https://brygk8u8b9.execute-api.us-east-1.amazonaws.com/dev?' + searchParams)
+    .then(response => response.json())
+    .then(data => setSongNames(data.body))
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4"> {groupName} </h1>
+      <ul className="list-disc pl-6">
+        {songNames.map((songName) => (
+          <li key={songName}> {songName} </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- app/src/Home.js
+++ app/src/Home.js
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const response = await fetch('https://brygk8u8b9.execute-api.us-east-1.amazonaws.com/dev')
-const res = await response.json()
-var groupNames = res.body
-
 const Home = () => {
+  const [groupNames, setGroupNames] = useState([]);
+  fetch('https://brygk8u8b9.execute-api.us-east-1.amazonaws.com/dev')
+    .then(response => response.json())
+    .then(data => setGroupNames(data.body))
+
   return (
     <div className="p-6">
       <ul className="list-disc pl-6">
         {groupNames.map((groupName) => (
           <li key={groupName}>
-            <Link to={`/k-line-distribution/${groupName}`} className="text-blue-600 hover:underline">
+            <Link to={`/k-line-distribution/${groupName.replace('/', '%2F')}`} className="text-blue-600 hover:underline">
               {groupName}
             </Link>
           </li>
